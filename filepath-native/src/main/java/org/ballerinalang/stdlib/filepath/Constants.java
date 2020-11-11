4397a08aaf1023694df5e9d030e9aa4ec5056d6e
ThishaniLucas <thishani@wso2.com>
2020-11-11 10:24:28 +0530

    Add runtime API changes

--- filepath-native/src/main/java/org/ballerinalang/stdlib/filepath/Constants.java
+++ filepath-native/src/main/java/org/ballerinalang/stdlib/filepath/Constants.java
@@ -20,8 +20,8 @@ package org.ballerinalang.stdlib.filepath;
 
 import io.ballerina.runtime.api.Module;
 
-import static io.ballerina.runtime.util.BLangConstants.BALLERINA_BUILTIN_PKG_PREFIX;
-import static io.ballerina.runtime.util.BLangConstants.ORG_NAME_SEPARATOR;
+import static io.ballerina.runtime.api.constants.RuntimeConstants.BALLERINA_BUILTIN_PKG_PREFIX;
+import static io.ballerina.runtime.api.constants.RuntimeConstants.ORG_NAME_SEPARATOR;
 
 /**
  * Constants for path package file functions.
--- filepath-native/src/main/java/org/ballerinalang/stdlib/filepath/nativeimpl/FilePathUtils.java
+++ filepath-native/src/main/java/org/ballerinalang/stdlib/filepath/nativeimpl/FilePathUtils.java
@@ -18,7 +18,7 @@
 
 package org.ballerinalang.stdlib.filepath.nativeimpl;
 
-import io.ballerina.runtime.api.StringUtils;
+import io.ballerina.runtime.api.utils.StringUtils;
 import io.ballerina.runtime.api.values.BString;
 import org.ballerinalang.stdlib.filepath.Constants;
 import org.ballerinalang.stdlib.filepath.Utils;

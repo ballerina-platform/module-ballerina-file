f4acdb9dba0235b105cf66ed42e64c6b14f75da9
Kalaiyarasi <ashakalai31@gmail.com>
2022-07-21 09:54:38 +0530

    Add class comments

--- native/src/main/java/io/ballerina/stdlib/file/transport/contractimpl/FileSystemServerConnectorImpl.java
+++ native/src/main/java/io/ballerina/stdlib/file/transport/contractimpl/FileSystemServerConnectorImpl.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2022, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package io.ballerina.stdlib.file.transport.contractimpl;
 
 import io.ballerina.stdlib.file.service.FSListener;
@@ -8,7 +26,9 @@ import org.wso2.transport.localfilesystem.server.exception.LocalFileSystemServer
 import java.util.Map;
 
 /**
+ * Implementation of the LocalFileSystemServerConnector interface.
  *
+ * @since 1.3.1
  */
 public class FileSystemServerConnectorImpl implements FileSystemServerConnector {
 

a2e53d769b47046b5652a207da5d3d0a477eef8e
ravinperera00 <bravinperera@gmail.com>
2024-10-03 16:12:17 +0530

    Migrate to Java 21

--- native/src/main/java/io/ballerina/stdlib/file/service/FSListener.java
+++ native/src/main/java/io/ballerina/stdlib/file/service/FSListener.java
@@ -18,40 +18,22 @@
 
 package io.ballerina.stdlib.file.service;
 
-import io.ballerina.runtime.api.PredefinedTypes;
 import io.ballerina.runtime.api.Runtime;
-import io.ballerina.runtime.api.async.StrandMetadata;
-import io.ballerina.runtime.api.creators.ValueCreator;
 import io.ballerina.runtime.api.types.MethodType;
-import io.ballerina.runtime.api.types.ObjectType;
-import io.ballerina.runtime.api.utils.StringUtils;
-import io.ballerina.runtime.api.utils.TypeUtils;
-import io.ballerina.runtime.api.values.BMap;
 import io.ballerina.runtime.api.values.BObject;
-import io.ballerina.runtime.api.values.BString;
-import io.ballerina.stdlib.file.utils.FileConstants;
-import io.ballerina.stdlib.file.utils.ModuleUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.wso2.transport.localfilesystem.server.connector.contract.LocalFileSystemEvent;
 import org.wso2.transport.localfilesystem.server.connector.contract.LocalFileSystemListener;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static io.ballerina.stdlib.file.service.DirectoryListenerConstants.FILE_SYSTEM_EVENT;
-import static io.ballerina.stdlib.file.service.DirectoryListenerConstants.RESOURCE_NAME_ON_MESSAGE;
-
 /**
  * File System connector listener for Ballerina.
  */
 public class FSListener implements LocalFileSystemListener {
 
-    private static final Logger log = LoggerFactory.getLogger(FSListener.class);
     private Runtime runtime;
     private Map<BObject, Map<String, MethodType>> serviceRegistry = new HashMap<>();
-    private static final StrandMetadata ON_MESSAGE_METADATA = new StrandMetadata(ModuleUtils.getModule().getOrg(),
-            ModuleUtils.getModule().getName(), ModuleUtils.getModule().getVersion(), RESOURCE_NAME_ON_MESSAGE);
 
     public FSListener(Runtime runtime) {
         this.runtime = runtime;
@@ -59,33 +41,14 @@ public class FSListener implements LocalFileSystemListener {
 
     @Override
     public void onMessage(LocalFileSystemEvent fileEvent) {
-        Object[] parameters = getJvmSignatureParameters(fileEvent);
         for (Map.Entry<BObject, Map<String, MethodType>> serviceEntry: serviceRegistry.entrySet()) {
             MethodType serviceFunction = serviceEntry.getValue().get(fileEvent.getEvent());
             if (serviceFunction != null) {
                 String functionName = serviceFunction.getName();
                 BObject service  = serviceEntry.getKey();
-                ObjectType type = (ObjectType) TypeUtils.getReferredType(TypeUtils.getType(service));
-                if (type.isIsolated() && type.isIsolated(functionName)) {
-                    runtime.invokeMethodAsyncConcurrently(service, functionName, null,
-                            ON_MESSAGE_METADATA, new DirectoryCallback(), null, PredefinedTypes.TYPE_NULL,
-                            parameters);
-                } else {
-                    runtime.invokeMethodAsyncSequentially(service, functionName, null,
-                            ON_MESSAGE_METADATA, new DirectoryCallback(), null, PredefinedTypes.TYPE_NULL,
-                            parameters);
-                }
+                runtime.call(service, functionName);
             }
         }
-    }
-
-    private Object[] getJvmSignatureParameters(LocalFileSystemEvent fileEvent) {
-        BMap<BString, Object> eventStruct = ValueCreator.createRecordValue(ModuleUtils.getModule(), FILE_SYSTEM_EVENT);
-        eventStruct.put(StringUtils.fromString(FileConstants.FILE_EVENT_NAME),
-                StringUtils.fromString(fileEvent.getFileName()));
-        eventStruct.put(StringUtils.fromString(FileConstants.FILE_EVENT_OPERATION),
-                StringUtils.fromString(fileEvent.getEvent()));
-        return new Object[] { eventStruct, true };
     }
 
     public void addService(BObject service, Map<String, MethodType> attachedFunctions) {
